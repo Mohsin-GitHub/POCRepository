eb02c3a6cf5f18769216f3f68c668665f7db0cb4
Mohsin-GitHub <soft.mohsin@gmail.com>
2017-02-11 23:35:24 +0800

    added new controller for bikelocation

---
+++ Swagger-CSharp/Swagger-CSharp/Swagger-CSharp/Controllers/BikeLocationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Swagger_CSharp.Models;
+using Swagger_CSharp.Providers;
+using Swagger_CSharp.Results;
+
+namespace Swagger_CSharp.Controllers
+{
+    public class BikeLocationController:ApiController
+    {
+        [HttpGet]
+        [ActionName("bikeLocation")]
+        public BikeLocationDTO GetBikeLocation(int bikeId)
+        {
+            return new BikeLocationDTO() { BikeId = bikeId, BikeLatitude = 20.5, BikeLongitude = 25.4 };
+        }
+    }
+}
---
+++ Swagger-CSharp/Swagger-CSharp/Swagger-CSharp/Models/BikeLocationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swagger_CSharp.Models
+{
+    public class BikeLocationDTO
+    {
+        public int BikeId { get; set;}
+        public double BikeLatitude { get; set; }
+        public double BikeLongitude { get; set; }
+    }
+}
