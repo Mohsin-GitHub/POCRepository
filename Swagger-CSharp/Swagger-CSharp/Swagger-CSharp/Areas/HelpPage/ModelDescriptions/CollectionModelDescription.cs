d13229178b72389b689b0a7b2a6f71418952fdc9
Mohsin-GitHub <soft.mohsin@gmail.com>
2017-02-11 23:13:07 +0800

    POC for Swagger UI

---
+++ Swagger-CSharp/Swagger-CSharp/Swagger-CSharp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Swagger_CSharp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Swagger-CSharp/Swagger-CSharp/Swagger-CSharp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Swagger_CSharp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
